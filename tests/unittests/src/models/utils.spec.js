6b18c7aec30346340fc5834475dede4509d3410c
Jonathan <jonathan.frere@gmail.com>
2017-10-21 17:08:02 +0100

    Add tests for model utilities

---
+++ tests/unittests/src/models/utils.spec.js
@@ -0,0 +1,19 @@
+const demand = require('must');
+const { required } = require('../../../../src/models/utils');
+
+describe('required', () => {
+
+    it('should return the correct value if the key is in the object', () => {
+        const obj = { testKey: Symbol() };
+
+        demand(required(obj, 'testKey')).to.equal(obj.testKey);
+    });
+
+    it('should throw an error if the key is not in the object', () => {
+        const obj = {};
+
+        demand(() => required(obj, 'testKey'))
+            .to.throw(Error, /testKey/);
+    })
+
+});
