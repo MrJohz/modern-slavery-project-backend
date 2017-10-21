0e3f49e4746ab6906cf3bec258cd2997397bbc78
Jonathan <jonathan.frere@gmail.com>
2017-10-21 17:11:40 +0100

    Ensure tests are run under the 'test' NODE_ENV

--- tests/unittests/src/models/utils.spec.js
+++ tests/unittests/src/models/utils.spec.js
@@ -1,19 +1,23 @@
 const demand = require('must');
 const { required } = require('../../../../src/models/utils');
 
-describe('required', () => {
+describe('models/utils', () => {
 
-    it('should return the correct value if the key is in the object', () => {
-        const obj = { testKey: Symbol() };
+    describe('required', () => {
 
-        demand(required(obj, 'testKey')).to.equal(obj.testKey);
+        it('should return the correct value if the key is in the object', () => {
+            const obj = { testKey: Symbol() };
+
+            demand(required(obj, 'testKey')).to.equal(obj.testKey);
+        });
+
+        it('should throw an error if the key is not in the object', () => {
+            const obj = {};
+
+            demand(() => required(obj, 'testKey'))
+                .to.throw(Error, /testKey/);
+        })
+
     });
-
-    it('should throw an error if the key is not in the object', () => {
-        const obj = {};
-
-        demand(() => required(obj, 'testKey'))
-            .to.throw(Error, /testKey/);
-    })
 
 });
