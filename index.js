069883a307c4c5bbca2d09141e10a2c68a483134
Jonathan <jonathan.frere@gmail.com>
2018-03-03 16:16:21 +0000

    Allow frontend bundle to be hosted by backend server

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const { server } = require('./src/server');
 
+server.register(require('inert'), err => {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/app/{param*}',
+        handler: {
+            directory: {
+                path: '../frontend/dist',
+                listing: true,
+                index: true,
+            }
+        }
+    })
+});
+
 server.start(err => {
-    if (err) console.error(err);
+    if (err) return console.error(err);
 
     console.log(`Server running at ${server.info.uri}`);
 });
