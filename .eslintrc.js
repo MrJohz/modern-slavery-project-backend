1af6aa9ffd1ce747811c885479be9f8fec91d941
Jonathan <jonathan.frere@gmail.com>
2018-04-23 13:14:24 +0100

    Add code coverage/linting tools

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "parserOptions": {
+        "ecmaVersion": "2017"
+    }
+};
